2d1a196541ca5fd41d4310efe58addd9a8d6b54d
Laura Fernanda Martinez Galindo <lf.martinezg1@uniandes.edu.co>
2024-10-30 09:05:03 -0500

    creo que ya

--- src/logica/Parcial.java
+++ src/logica/Parcial.java
@@ -3,6 +3,8 @@ package logica;
 import java.util.ArrayList;
 
 public class Parcial {
+	//me garantizan que todos los que entran son enteros
+	//me garantizan que a diferente de 0 porque si no no seria cuadratica, entonces, no debo verificar división por 0
 	
 	public ArrayList<Double> calcularRaices(int a, int b, int c) throws Exception{
 		
